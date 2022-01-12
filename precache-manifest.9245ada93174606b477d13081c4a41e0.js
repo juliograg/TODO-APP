23ff50c2a6de7e5d6309fd6ace7754f02f55d26a
Julio Gragirena <juliogragirena@outlook.es>
2022-01-11 21:56:47 -0500

    Updates

---
+++ precache-manifest.9245ada93174606b477d13081c4a41e0.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ca73a0e643302bc581fedaafb64a7398",
+    "url": "/TODO-APP/index.html"
+  },
+  {
+    "revision": "7cff0611861f76ed9106",
+    "url": "/TODO-APP/static/css/main.1fa30622.chunk.css"
+  },
+  {
+    "revision": "dfa69edea5e6cc6f2a47",
+    "url": "/TODO-APP/static/js/2.90272c86.chunk.js"
+  },
+  {
+    "revision": "7cff0611861f76ed9106",
+    "url": "/TODO-APP/static/js/main.f7ea768c.chunk.js"
+  },
+  {
+    "revision": "4a73c7523bc89dec406d",
+    "url": "/TODO-APP/static/js/runtime~main.98d59206.js"
+  }
+]);
